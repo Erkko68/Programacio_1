e89c60576d69416959fb955dbf2f6adddc7c3ce4
Èric Bitrià Ribes <47712373+Erkko68@users.noreply.github.com>
2022-10-22 20:05:38 +0200

    Added comments and remaining functionalities.

--- Practiques/pract1/ex1.cpp
+++ Practiques/pract1/ex1.cpp
@@ -1,98 +1,264 @@
-//Pràctica ruleta//
-//definir llibreries standard
+//Practica 1: Crear un programa que simuli la funcionalitat d'una ruleta de casino.
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+//inlcoure llibreries estandard
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//defiinir variables globals
+
+int capital=1000; 	//definir capital inicial
+int mode;		//definir mode de joc
+int aposta;		//definir valor de la aposta en euros
+int guess;		//numero que tria el jugador
+int profit;		//guanys
+int ruleta;		//(Nombre generat aleatoriament per la ruleta)
+int perd,guanya; 	//variables per registrar les vegades que guanya o perd.
 
 
 //inicialització generacio de nombres aleatoris.
 
-int random_numb(int n){
+void random_numb(){
 
 	srand(time(NULL));
-	n = rand() % 36 + 1;
+	ruleta = rand() % 36 + 1;
 	
-	return(n);
 }
 
-//Funció mostrar enunciats
+//funció per mostrar la user interface.
 
-void welcome_print(){ 
+void ui_print(){
 
-	printf("Benvingut al joc de la ruleta!\n");
-
-}
-
-//Funció calcular capital
-
-int capital_cal(int mode, bool result,int aposta){
-
-	int capital_inicial=1000;
-	int capital_calc = capital_inicial;
-
-	//Mode endevinar numero
-
-	if(mode==1){ 
-		if(result==1){	//Guanyar implica multiplicar la aposta per 36 restant la propia aposta.
-		
-			capital_calc=(capital_calc)+(aposta*36-aposta);
-			
-			return(capital_calc);
-		
-		}else if(result==0){ //perdre es restar la aposta al capital
-		
-			capital_calc=capital_calc-aposta;
-			
-			return(capital_calc);
-		
-		}
-		
-	//Mode endevinar parell o imparell	
-
-	}else if(mode==2){
+	printf("Tens un capital de %i Euros. A quin joc vols jugar?\n",capital);
+	printf("-Prem 0 per sortir\n");
+	printf("-Prem 1 per jugar a endevinar el numero de l'1 al 36\n");
+	printf("-Prem 2 per jugar a endevinar si sera parell o imparell\n");
+	printf(">");
 	
-		if(result==1){ //guanyar suma el valor de la aposta al capital en aquell moment
-		
-			capital_calc=capital_calc+aposta;
-			
-			return(capital_calc);
-		
-		}else if(result==0){	//perdre resta el valor de la aposta al capital en aquell moment
-		
-			capital_calc=capital_calc-aposta;
-			
-			return(capital_calc);
-		
-		}
-	
-	}
-
-	return 0;
+	scanf("%i",&mode); //determinar quin joc vol jugar el jugador guardant el nombre a la variable "mode".
 
 }
 
-void mostrar_capital(int capital_final){
+//Mode de joc 1, endevinar el numero de la ruleta.
 
-	printf("Tens un capital de %i Euros.",capital_final);
-
-
-}
 void mode_1(){
 
+	//mostrar ui
 	
-
+	printf("Tens un capital de %i Euros, quina quantitat vols apostar? \n",capital); 
+	printf(">");
+	scanf("%i",&aposta);
+			
+		while(aposta>capital || aposta<0){ //verificar que el jugador té prous diners
+				
+			printf("No tens prous diners!\n");
+			printf(">");
+			scanf("%i",&aposta);
+				
+		}
+				
+		printf("-Introdueix el numero per la teva aposta (de l'1 al 36):\n"); //mostrar ui
+		printf(">");
+			
+		scanf("%i",&guess);
+			
+			while(guess<1 || guess>36){ //verificar que el valor introduit esta dins el rang de la ruleta
+				
+				printf("Valor incorrecte!\n");
+				printf(">");
+				scanf("%i",&guess);
+				
+			}
+				
+		printf("Has apostat pel numero %i i ha sortit %i\n",guess,ruleta); //mostrar resultats
+			
+			
+			if(guess==ruleta){ //si guanya calcular guanys
+			
+				profit = (aposta*36)-aposta; 
+				
+				printf("Has guanyat %i Euros!\n",profit);
+				
+				capital = capital+profit;
+				
+				guanya++;
+				
+			}else{ //si perd calcular perdues
+				
+				printf("Has perdut %i Euros!\n",aposta);
+				
+				capital = capital-aposta;
+				
+				perd++;
+				
+			}
+			
+//tornar a demanar al jugador quin mode vol jugar en cas que no ho hagi perdut tot. I generar un nou numero aleatori.
+			
+		if(capital>0){ 
+		
+			random_numb();
+			ui_print();
+			
+		}
 
 }
+
+//definir la funcio mode_2, per a executar el segon mode de joc de endevinar si el nombre es parell o imparell
+
+void mode_2(){
+
+//mostrar ui
+
+	printf("Tens un capital de %i Euros, quina quantitat vols apostar? \n",capital);
+	printf(">");
+	scanf("%i",&aposta);
+			
+		while(aposta>capital || aposta<0){ //verificar que el jugador té prous diners
+				
+			printf("No tens prous diners!\n");
+			printf(">");
+			scanf("%i",&aposta);
+				
+		}
+		
+	printf("Prem 1 per apostar a numero imparell o 2 per apostar a numero parell:\n");
+	printf(">");
+			
+	scanf("%i",&guess);
+			
+		while(guess!=1 && guess!=2){ //verificar el valor introduit
+				
+			printf("Valor incorrecte!\n");
+			printf(">");
+			scanf("%i",&guess);
+				
+		}
+		
+		//regla de la ruleta, si surt el numero 0 el jugador o perd tot.
+		
+		if(ruleta==0){
+		
+			printf("Has perdut %i Euros!\n",aposta);
+				
+			capital = capital-aposta;
+				
+			perd++;
+		}
+		
+		if(guess==1){ //Aposta per imparell
+	
+			printf("Has apostat per imparell i ha sortit %i\n",ruleta);
+		
+			
+		
+			if(ruleta % 2 == 0){  //determinar si el nombre de la ruleta es parell mirant si obtenim residu.
+		
+				printf("Has perdut %i Euros!\n",aposta);
+				
+				capital = capital-aposta;
+				
+				perd++;
+		
+			}else{
+			
+				printf("Has guanyat %i Euros!\n",aposta);
+				
+				capital = capital+aposta;
+				
+				guanya++;
+			
+			}
+		
+		}else if(guess==2){ //aposta parell
+	
+			printf("Has apostat per imparell i ha sortit %i\n",ruleta);
+		
+			if(ruleta % 2 == 0){ //determinar si el nombre de la ruleta es parell mirant si obtenim residu.
+		
+				printf("Has guanyat %i Euros!\n",aposta);
+				
+				capital = capital+aposta;
+				
+				guanya++;
+		
+			}else{
+			
+				printf("Has perdut %i Euros!\n",aposta);
+				
+				capital = capital-aposta;
+				
+				perd++;
+			
+			}
+		}
+		
+//tornar a demanar al jugador quin mode vol jugar en cas que no ho hagi perdut tot. I generar un nou numero aleatori.
+	
+	if(capital>0){
+		
+		random_numb();
+		ui_print();
+			
+	}
+}
+
+
+//funcio principal
 
 int main(){
 
-	int n = random_numb(n); //definir el nombre aleatori
-	int capital_final;	//definir variable on guardar el capital final
+	printf("Benvingut al joc de la Ruleta!\n");  //Mostrar en pantalla la funcionalitat del programa.
 	
-	capital_final = capital_cal(1,1,200);
+	random_numb(); //generar nombre aleatori
 	
-	welcome_print();
-	mostrar_capital(capital_final);
+	ui_print();	//Cridar a la funció ui_print per a mostrar els modes de joc
 	
+	//mentres el jugador no premi 0 o el seu capital sigui 0 es seguira executant.
+	
+	while((mode!=0) && (capital>0)){ 
+		
+		//verificar que s'ha seleccionat un mode de joc valid.
+		
+		while(mode!=0 && mode!=1 && mode!=2){
+		
+			printf("Mode incorrecte!\n");
+			printf(">");
+			scanf("%i",&mode);
+		
+		}
+		
+		while(mode==1 && capital>0){
+
+			mode_1(); 		//En cas de seleccionar mode 1 cridar a la funcio mode_1();
+
+		}
+		
+		while(mode==2 && capital>0){
+
+			mode_2();		//En cas de seleccionar mode 2 cridar a la funcio mode_2();
+
+		}
+		
+
+	}
+	
+	//indicar al jugador resultats finals.
+	
+	if(capital<=0){
+	
+		printf("Ho has perdut tot!\n"); //si el capital es igual a 0 mostrar que ho ha perdut tot.
+	
+	}else{
+	
+	printf("El teu capital final es de %i Euros.\n",capital); //mostrar capital final
+	
+	}
+	
+	//mostrar el nombre de vegades que el jugador ha guanyat i perdut.
+	
+	printf("Has guanyat %i partides i n'has perdut %i.\n",guanya,perd); 
+
+
 }
