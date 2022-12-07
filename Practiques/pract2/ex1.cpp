72356a22b193ac1b93b7679123460c7957e5a83b
Èric Bitrià Ribes <47712373+Erkko68@users.noreply.github.com>
2022-12-07 20:26:19 +0100

    Finish ex1

--- Practiques/pract2/ex1.cpp
+++ Practiques/pract2/ex1.cpp
@@ -7,154 +7,145 @@
 #define MAX_WORDS 10
 #define COD_LENGHT 26
 
-void compare();
+//Define variable number anagrams
 
-//variables globals
+int angr = 0;
 
-char search_words[MAX_WORDS][WORD_LENGHT+1];
-char word[WORD_LENGHT+1];
+//define functions
 
-int codified_words[MAX_WORDS+1][COD_LENGHT]={{}};
-int codified_word[COD_LENGHT]={};
+char codify(char arr[WORD_LENGHT+1],int i);
+char compare(char arr[WORD_LENGHT+1]);
+void read_search();
+void read();
+void process();
 
+char words[MAX_WORDS][WORD_LENGHT];
+int codified_words[MAX_WORDS][COD_LENGHT]={{}};
+
+//Read and store serach words into words array with a maximum of 10 words.
 
 void read_search(){
-
-	char letter,letter_prev = ' ';
 	
-	int i=0;
-	int j=0;
+	char letter;
 	
+	int i=0,j=0;
+	
+	
+	printf("Anagrames a buscar: \n");
 	scanf("%c",&letter);
-	
-	while(letter != '.' and i<=MAX_WORDS and j<WORD_LENGHT){ 
-		if(isalpha(letter)){ 			//if the char is a letter add into the the array
-			search_words[i][j] = letter;	//
-			j++;				//increment letter position
-		}else if(!isalpha(letter) and isalpha(letter_prev)){ //if the next char is not a letter switch to new word.
-			search_words[i][j+1]= '\0';
-			i++;
-			j=0;
-		}
-		letter_prev = letter;
-		scanf("%c",&letter);	
-	}
-	//debug only show arrays
-	
-	//for(int i=0;i<MAX_WORDS;i++){
-	//	if(search_words[i]!=" "){
-	//		printf("%s\n",search_words[i]);
-	//	}
-	//}
-}
-
-void read_word(){
-
-	char letter,letter_prev = ' ';
-	
-	int i=0;
-	
-	scanf("%c",&letter);
-	
-	while(letter != '.' and i<WORD_LENGHT){ 
-		if(isalpha(letter)){ 		
-			word[i] = letter;
-			i++;			
-		}else if(!isalpha(letter) and isalpha(letter_prev)){
-			word[i+1]= '\0';
-			compare();
-			
-		}
-		letter_prev = letter;
+		
+	while(letter!='.' and i<MAX_WORDS and j<WORD_LENGHT){ //store each letter in the correspondig array postion
+		if(isalpha(letter)){
+			words[i+1][j]=letter;
+			j++;
+		}else if(!isalpha(letter)){
+			words[i+1][j+1]='\0';
+			if(isalpha(words[i+1][0])){ //if the previous string is not empty use next row(used to ignore non letter chars)
+				i++;
+			}
+			j=0; //reset array pos
+		}			
 		scanf("%c",&letter);
 	}
-	compare();
-	
-	
 }
 
-int codify(char arr[WORD_LENGHT+1]){
+//Read and store one word from the given text.
+
+void read(){
+
+	char letter_text;
+
+	int j=0;
+	
+	scanf("%c",&letter_text);
+	
+	while(letter_text!='.' and j<WORD_LENGHT){
+		if(isalpha(letter_text)){
+			words[10][j]=letter_text;
+			j++;
+		}else if(!isalpha(letter_text)){
+			words[10][j+1]='\0';
+			j=0;
+			
+			if(isalpha(words[10][0])){	//when the next char is not a letter means the word has ended so 
+				compare(words[10]);		//it calls the function compare to process it
+			}
+			
+		}	
+		scanf("%c",&letter_text);
+	
+	}
+	if(isalpha(words[10][0])){ //used if the last word is the anagram of the search one.
+		compare(words[10]);
+	}
+}
+
+//codify given character array into an integer string stored in codified_words.
+
+char codify(char arr[WORD_LENGHT+1],int i){
 
 	int pos = 0;
 	
-	for(int j=0;j<=WORD_LENGHT;j++){
+	//EMPTY LIST
 	
-		if(tolower(arr[j])>='a' and tolower(arr[j])<='z'){
-			pos = 'z'-tolower(arr[j]);
-			codified_word[pos]++;	
-		}
+	memset(codified_words[i],0,sizeof(codified_words[i]));
+	
+	for(int j=0;j<COD_LENGHT;j++){
+		pos = 'z'-tolower(arr[j]);
+		codified_words[i][pos]++;
 	}
-	printf("%s",arr);
-
+		
 	return 1;
 }
 
-void store_search(){
+//function to compare each word of the text
 
-	//store search words codified
-	
-	for(int i=0;i<MAX_WORDS;i++){
-		codify(search_words[i]);
-		for(int j=0;j<COD_LENGHT;j++){
-			codified_words[i][j] = codified_word[j];
-		}
-		memset(codified_word,0,sizeof(codified_word)); //reset list
-		
-	}
-	
-	//for(int i = 0;i<MAX_WORDS;i++){
-	//	for(int j = 0; j<COD_LENGHT;j++){
-	//		printf("%i",codified_words[i][j]);
-	//	}
-	//	printf("\n");
-	//}
-	
-}
+char compare(char arr[WORD_LENGHT+1]){
 
-int store(int arr[WORD_LENGHT+1]){
+	codify(arr,10);
 
-	//store word codified
-
-	for(int i=0;i<WORD_LENGHT;i++){
-		codified_words[10][i] = arr[i];
-	}
-	
-	return 0;
-
-}
-
-void compare(){
-
-	int j;
 	int c=0;
 	
-	codify(word);
-	
-	for(int i=0;i<MAX_WORDS;i++){
+	int i=0,j=0;
+
+	for(i=0;i<MAX_WORDS;i++){
 		
 		for(j=0;j<COD_LENGHT;j++){
-        	if(codified_word[j]==codified_words[i][j]){
+        	if(codified_words[i][j]==codified_words[10][j]){
 				c++;
 			}
 		}
-		
 		if(c==j){
-			printf("Anagrama de la paraula \"%s\" trobat: \"%s\".\n",search_words[i],word);
+			printf("Anagrama de la paraula \"%s\" trobat: \"%s\".\n",words[i],arr);
+			angr++;
 		}
 		c=0;	
 	}
-	memset(word,0,sizeof(word));
-	memset(codified_word,0,sizeof(codified_word));
 	
+	memset(words[10],0,sizeof(words[10])); //reset word
 	
+	return 1;
 }
 
-int main(){
+//
+void process(){
 
 	read_search();
 	
-	store_search();
+	for(int i=0;i<MAX_WORDS;i++){
+		codify(words[i],i);
+	}
 	
-	read_word();
+	read();
+	
+	printf("S'han trobat %i anagrames\n",angr);
+}
+
+
+
+
+int main(){
+
+	process();
 		
 }
